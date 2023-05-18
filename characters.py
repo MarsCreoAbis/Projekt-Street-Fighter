3d1230889c8a949e45b6f301797ee76df472525e
MarsCreoAbis <131890978+MarsCreoAbis@users.noreply.github.com>
2023-05-18 20:15:23 +0200

    hermetyzacja pól w characters.py

--- characters.py
+++ characters.py
@@ -4,35 +4,35 @@ class Character():
     def __init__(self, hp, stamina, speed , size, sprite, player):
         
         ### Przypisuje postaci do konkretnego gracza
-        self.controlled_by = player
+        self.__controlled_by = player
         ###
         
         ### Ustawia początkową pozycję postaci, wartości tymczasowe zmienię jak będzie już zrobione środowisko gry
-        if self.controlled_by == 0:
+        if self.__controlled_by == 0:
             self.position = [15,0]
         else:
             self.position = [31, 0]
         ###
         
         ### Ta część będzie wyznaczać hitboxy postaci, póki co jeszcze nie zdefiniowane
-        self.heigh, self.width = size, size/2
+        self.__heigh, self.__width = size, size/2
         #self.upper_hitbox = Hitbox(self.heigh, self.width)
         #self.lower_hitbox = Hitbox(self.heigh, self.width)
         ###            
             
         ### Ta część ustawia makysmalne i początkowe statystyki postaci
-        self.max_hit_points = hp
-        self.hit_points = hp
-        self.max_stamina = stamina
-        self.stamina = stamina
-        self.speed = 5 - speed ### 
+        self.__max_hit_points = hp
+        self.__hit_points = hp
+        self.__max_stamina = stamina
+        self.__stamina = stamina
+        self.__speed = speed ### 
         ###
         
         ### Będzie przypisywać konkretną grafikę psotaci, póki co nie robi nic
-        self.sprite = sprite
+        self.__sprite = sprite
         ###
 
         ### Tu będą różna potencjalne, stany postaci
-        self.stun = 0
-        self.stunned = False
+        self.__stun = 0
+        self.__stunned = False
         ###
