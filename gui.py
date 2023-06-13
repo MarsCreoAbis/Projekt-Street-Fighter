3cabf532a434bba41ae592a78486020cba885ea6
Elmira Aliyeva <elmira.aliyeva.b@gmail.com>
2023-06-13 19:48:34 +0200

    dodalam opcje nowa gra i wyjscie (wyglada okropnie wiem)

--- gui.py
+++ gui.py
@@ -21,6 +21,8 @@ class Healtbar():
         self.koniec_timera = False  # to zeby rozumiec ze timer się zakończył
 
         self.winner = None # tego potrzebuje zeby w koncu rundy sie pojawialo okienko z informacja kto tam wygral 
+        #self.pokaz_opcje = False  # to ma sie zmienic na True kiedy timer sie skonczy, dla wyswirtlania opcji
+
 
     def update(self, current_health_player1, current_health_player2):
         self.current_health_player1 = current_health_player1
@@ -29,6 +31,8 @@ class Healtbar():
         czas_uplyniety = time.time() - self.start_time
         if  czas_uplyniety >= self.dlugosc_rundy:
             self.koniec_timera = True
+            #self.pokaz_opcje = True  # tu sie wlasnie zmienia na True
+
 
 
         # tu sprawdzam czy czas timera się skończył
@@ -68,21 +72,35 @@ class Healtbar():
             pygame.draw.rect(screen, (255, 0, 0), wynik_rect)
             screen.blit(wynik_render, wynik_rect.topleft)
 
-        # pozycja timera
-        timer_x = (self.x + self.x + 350) // 2  # tu licze srodek healtbara 
-        timer_y = self.y + 90  # tu zadaje pozycje
 
-        # czas uplyniety
-        czas_uplyniety = time.time() - self.start_time
-        czas_pozostaly = max(0, self.dlugosc_rundy -  czas_uplyniety)
-        timer_text = f"Time: {int(czas_pozostaly)}"
-        render = font.render(timer_text, True, (0, 0, 0))
-        timer_rect = render.get_rect(center=(timer_x, timer_y))
-        screen.blit(render, timer_rect)
+            # wyswietlanie opcji nowa gra i wyjsc kiedy timer sie skonczy
+            option_font = pygame.font.Font('freesansbold.ttf', 24)
+            option_text_1 = "1. Nowa gra"
+            option_text_2 = "2. Wyjscie"
+            option_render_1 = option_font.render(option_text_1, True, (0, 0, 0))
+            option_render_2 = option_font.render(option_text_2, True, (0, 0, 0))
+            option_rect_1 = option_render_1.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2 + 50))
+            option_rect_2 = option_render_2.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2 + 80))
+            screen.blit(option_render_1, option_rect_1.topleft)
+            screen.blit(option_render_2, option_rect_2.topleft)
 
-        # Sprawdzanie, czy timer się zakończył
-        if self.koniec_timera:
-            koniec_gry_text = font.render("Koniec gry!", True, (255, 0, 0))
-            screen.blit(koniec_gry_text, (self.x, self.y + 40))
+
+        else:
+            # pozycja timera
+            timer_x = (self.x + self.x + 350) // 2  # tu licze srodek healtbara 
+            timer_y = self.y + 90  # tu zadaje pozycje
+
+            # czas uplyniety
+            czas_uplyniety = time.time() - self.start_time
+            czas_pozostaly = max(0, self.dlugosc_rundy -  czas_uplyniety)
+            timer_text = f"Time: {int(czas_pozostaly)}"
+            render = font.render(timer_text, True, (0, 0, 0))
+            timer_rect = render.get_rect(center=(timer_x, timer_y))
+            screen.blit(render, timer_rect)
+
+            # Sprawdzanie, czy timer się zakończył
+            if self.koniec_timera:
+                koniec_gry_text = font.render("Czas sie skonczyl", True, (255, 0, 0))
+                screen.blit(koniec_gry_text, (self.x, self.y + 40))
 
     
